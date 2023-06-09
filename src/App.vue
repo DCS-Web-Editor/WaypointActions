6ccebc10c7717044bb36c6d13435f0c6d27f155f
EMcNugget <ethan.d.seys@gmail.com>
2023-06-09 13:27:23 -0500

    intial formation hook

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     :theme-overrides="selectedTheme === 'Dark' ? themeOverrides : null"
   >
     <div>
-      <ActionsList />
     </div>
   </n-config-provider>
 </template>
@@ -14,7 +13,6 @@ import type { GlobalTheme, GlobalThemeOverrides } from "naive-ui";
 import { NConfigProvider } from "naive-ui";
 import { useThemeStore } from "./stores/theme";
 import { ref } from "vue";
-import ActionsList from "./components/ActionsList.vue";
 
 const themeStore = useThemeStore();
 const theme = ref<GlobalTheme>(themeStore.theme);
--- src/components/ActionItem.vue
+++ src/components/ActionItem.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="flex flex-row text-xs">
-    <p class="p-2 pr-0">{{ props.index }}</p>
-    <p class="p-2">{{ props.data }}</p>
+    <p class="p-2 pr-0">{{ props.index + 1}}</p>
+    <p class="p-2">{{ concat(props.option, props.value) }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
   index: number;
-  data: string;
+  option: string;
+  value: string;
 }>();
+
+const concat = (option: string, value: string) => {
+  return `${value} = ${option}`;
+};
 </script>
