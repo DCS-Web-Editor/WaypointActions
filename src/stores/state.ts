42efc2b1b0af445f21141538bfd3caf700cb989b
EMcNugget <ethan.d.seys@gmail.com>
2023-06-10 22:34:29 -0500

    Added state

---
+++ src/stores/state.ts
@@ -0,0 +1,14 @@
+import type { Tasks } from "../types/Tasks";
+import { defineStore } from "pinia";
+
+export const useTasksStore = defineStore({
+  id: "tasks",
+  state: () => ({
+    tasks: [] as Tasks[],
+  }),
+  actions: {
+    setTasks(tasks: Tasks[]) {
+      this.tasks = tasks;
+    },
+  },
+});
