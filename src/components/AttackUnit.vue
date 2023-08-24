e9c747f7a075df5a31914fd99951566a7eedf512
EMcNugget <ethan.d.seys@gmail.com>
2023-08-24 13:19:34 -0500

    Added AttackUnit component functionality

--- src/components/AttackUnit.vue
+++ src/components/AttackUnit.vue
@@ -1,31 +1,48 @@
 <template>
   <slot name="above"></slot>
   <n-form-item label="Weapon" label-placement="left">
-    <n-select :options="weaponOptions" />
+    <n-select
+      v-model:value="weapon"
+      :options="weaponOptions"
+      @update:value="$emit('update:weapon', $event)"
+    />
   </n-form-item>
   <n-form-item label="Rel Qty" label-placement="left">
-    <n-select />
+    <n-select v-model:value="relQty" />
   </n-form-item>
   <div class="flex">
     <n-form-item class="mr-5" label="Max Attack Qty" label-placement="left"
-      ><n-checkbox />
+      ><n-checkbox v-model:checked="maxAtkQtyEnabled" />
     </n-form-item>
-    <n-input-number class="flex-1" :min="1" :max="1000" />
+    <n-input-number
+      class="flex-1"
+      :min="1"
+      :max="1000"
+      v-model:value="maxAtkQty"
+      :disabled="!maxAtkQtyEnabled"
+    />
     <n-form-item class="ml-5" label="Group Attack" label-placement="left"
-      ><n-checkbox />
+      ><n-checkbox v-model:checked="groupAtk" />
     </n-form-item>
   </div>
   <n-form-item label="Direction From" label-placement="left">
-    <n-input-number :min="0" :format="compassDir" class="w-full">
+    <n-input-number v-model:value="directionFrom" :min="0" :format="compassDir" class="w-full">
       <template #suffix>°</template>
     </n-input-number>
   </n-form-item>
   <div class="flex">
-    <n-form-item class="flex-none mr-5" label="Altitude Above" label-placement="left"
-      ><n-checkbox />
+    <n-form-item class="flex-none mr-5" label="Altitude Above" label-placement="left">
+      <n-checkbox v-model:checked="altitudeAboveEnabled" />
     </n-form-item>
-    <n-slider :step="1" :min="0" :max="62336" class="w-full mr-10 mt-2"> </n-slider>
-    <n-input-number :step="1" :min="0" :max="62336" class="w-full">
+    <n-slider
+      :step="1"
+      :min="0"
+      :max="62336"
+      class="w-full mr-10 mt-2"
+      v-model:value="altitudeAbove"
+    >
+    </n-slider>
+    <n-input-number :step="1" :min="0" :max="62336" class="w-full" v-model:value="altitudeAbove">
       <template #suffix>ft</template>
     </n-input-number>
   </div>
@@ -35,8 +52,52 @@
 <script setup lang="ts">
 import { NFormItem, NCheckbox, NSelect, NInputNumber, NSlider } from "naive-ui";
 import { getWeaponOptions } from "../utils/utils";
-import { computed } from "vue";
+import { computed, ref, toRef, watch } from "vue";
 import { useEntryStore } from "../stores/entryState";
+
+const props = defineProps<{
+  weapon: number;
+  relQty: number;
+  maxAtkQty?: number;
+  groupAtk: boolean;
+  directionFrom: number;
+  altitudeAbove?: number;
+}>();
+
+const weapon = toRef(props.weapon);
+const relQty = toRef(props.relQty);
+const maxAtkQty = toRef(props.maxAtkQty);
+const groupAtk = toRef(props.groupAtk);
+const directionFrom = toRef(props.directionFrom);
+const altitudeAbove = toRef(props.altitudeAbove);
+
+const maxAtkQtyEnabled = ref(false);
+
+watch(
+  maxAtkQty,
+  (val) => {
+    if (val) {
+      maxAtkQtyEnabled.value = true;
+    } else {
+      maxAtkQtyEnabled.value = false;
+    }
+  },
+  { immediate: true },
+);
+
+const altitudeAboveEnabled = ref(false);
+
+watch(
+  altitudeAbove,
+  (val) => {
+    if (val) {
+      altitudeAboveEnabled.value = true;
+    } else {
+      altitudeAboveEnabled.value = false;
+    }
+  },
+  { immediate: true },
+);
 
 const entry = useEntryStore();
 
@@ -56,4 +117,13 @@ const compassDir = (wind: number | null): string => {
 const weaponOptions = computed(() =>
   getWeaponOptions(entry.getUnit(), entry.getTargetGroup(), entry.getTaskCatagory()),
 );
+
+defineEmits([
+  "update:weapon",
+  "update:relQty",
+  "update:maxAtkQty",
+  "update:groupAtk",
+  "update:directionFrom",
+  "update:altitudeAbove",
+]);
 </script>
